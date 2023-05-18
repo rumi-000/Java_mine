3646b9e3fe8de1c4e93e66136dc18507cbc002ff
rumi-000 <rumi.sato9797@gmail.com>
2023-05-18 08:10:06 +0000

    6章

---
+++ jQuery-Lesson/app.js
@@ -0,0 +1,39 @@
+// $(function(){
+//   $('.box1').css({
+//   // CSSのスタイルをjQueryで変更するときは、「CSSメソッド」を使う
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// $(function(){
+//   $('.box1').slideDown();
+// });
+// // 上から表示させる
+
+// $(function(){
+//   $('.box1').slideUp();
+// });
+// // 下から上へ画像が消える
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+// ↓
+
+// $(function(){
+//   $('.box1').hide();
+// });
+// //jQuery要素で画像を消した。
+
+// $('.セレクタ名').イベント名(function(){
+//   イベント発生時に行われる処理
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+// });
