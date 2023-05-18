aa62d194233d79f4765fc008d271f0136298f023
rumi-000 <rumi.sato9797@gmail.com>
2023-05-18 04:49:57 +0000

    3章

--- vendor/index/app.js
+++ vendor/index/app.js
@@ -84,8 +84,55 @@
 // var nickname = "ichiro"
 // console.log(nickname)
 
-// varによる再代入
-var nickname = "taro"
-console.log(nickname)
-nickname = "jiro"
-console.log(nickname)
+
+// // グローバルスコープ、どこでも参照できる
+// var str = "webcamp" 
+
+// // 関数スコープ。foo関数内で参照できる
+// function foo() {
+//   console.log(str)
+//   var y = "hello" 
+// }
+
+// // console.log(str)→webcampと出てくる
+// // console.log(y)→yが定義されていないというエラーが出てくる
+// foo()
+// console.log(str)
+
+
+// function foo() {
+//   let x = "webcamp"
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+//   // foo()→関数外でやるとエラー、今回の様に関数内だとエラーにならない。
+// }
+
+
+// for (let i = 0; i < 10; i++) {     
+//   console.log(i) 
+// }
+
+// console.log(i)→関数外での参照をしたのでエラーになる。
+
+
+
+// // Aはグローバルスコープである
+// var str = "webcamp A"
+
+// // 変数内で使用されている宣言の巻き上げが起こる。=関数冒頭で変数を宣言したことになる=Bが宣言となる。
+// function foo() { 
+//   console.log(str)
+//   var str = "dmm webcamp B"
+//   console.log(str)
+// }
+
+// foo()
+
+
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
