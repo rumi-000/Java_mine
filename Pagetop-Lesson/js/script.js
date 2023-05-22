7f4b1e0291224c62cd0554726f58fb49d54aba7e
rumi-000 <rumi.sato9797@gmail.com>
2023-05-22 04:51:18 +0000

    9章おわり

---
+++ Pagetop-Lesson/js/script.js
@@ -0,0 +1,19 @@
+// $(function() {
+//   $('#back a').on('click',function(event){
+//   //#back内のaタグがクリックされたときの処理
+//   //$('#id名 要素名')と指定すると、id内の子要素に対してのみ指定されます。
+//   // function(event)のeventはイベントオブジェクト
+  
+//     $('body, html').animate({
+//     // animate() は、アニメーション効果を設定するjQueryの関数
+    
+//       scrollTop:0
+//     }, 800);
+//     //変化対象のプロパティ名:変化値
+//     // }, アニメーションの動作時間);  
+    
+//     event.preventDefault();
+//     // event.preventDefault(); は、aタグの機能を無効にする
+    
+//   });
+// });
